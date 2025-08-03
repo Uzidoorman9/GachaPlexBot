83ce09e62157d74d5ab607d4c35d2977846e409a
Uzi <timothymay2011@gmail.com>
2025-08-03 05:25:06 -0400

    Update bot.py

--- discord-bot-factory/bot-template/bot.py
+++ discord-bot-factory/bot-template/bot.py
@@ -1,1 +1,22 @@
+import discord
+import json
 
+with open("config.json") as f:
+    cfg = json.load(f)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+prefix = cfg["prefix"]
+
+@client.event
+async def on_ready():
+    print(f"Bot is online as {client.user}")
+
+@client.event
+async def on_message(msg):
+    if msg.author.bot or not msg.content.startswith(prefix):
+        return
+    if msg.content == f"{prefix}ping":
+        await msg.channel.send("Pong!")
+
+client.run(cfg["token"])
